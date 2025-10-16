10288f012e1831541a877f218a3566562f56980a
TVAn-yeet <24020005@vnu.edu.vn>
2025-10-16 16:45:00 +0700

    Tạo màn hình menu cơ bản (test.mp4 là vid thử) Thêm nội dung vào menu.fx Thêm dependencies javafx-media Thêm nội dung hàm main

--- src/main/java/com/nhom12/arkanoid/Main.java
+++ src/main/java/com/nhom12/arkanoid/Main.java
@@ -1,4 +1,23 @@
 package com.nhom12.arkanoid;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/view/menu.fxml")
+        );
+        Scene scene = new Scene(loader.load(), 1280, 720);
+        stage.setScene(scene);
+        stage.setTitle("Arkanoid Menu");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- src/main/java/com/nhom12/arkanoid/controller/MenuController.java
+++ src/main/java/com/nhom12/arkanoid/controller/MenuController.java
@@ -1,4 +1,32 @@
 package com.nhom12.arkanoid.controller;
 
-public class MenuController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    @FXML
+    private MediaView mediaView;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        URL mediaUrl = getClass().getResource("/test.mp4");
+        Objects.requireNonNull(mediaUrl, "Video file not found!");
+
+        Media media = new Media(mediaUrl.toExternalForm());
+        MediaPlayer player = new MediaPlayer(media);
+        player.setCycleCount(MediaPlayer.INDEFINITE);
+        player.setOnEndOfMedia(() -> player.seek(Duration.ZERO));
+        player.setAutoPlay(true);
+
+        mediaView.setMediaPlayer(player);
+    }
 }
