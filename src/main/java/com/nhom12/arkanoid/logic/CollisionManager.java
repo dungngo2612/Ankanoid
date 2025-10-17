7ba9bfc6cb171fd7fabc6262dd429aefbdef8df8
hungdo2006 <24020149@vnu.edu.vn>
2025-10-17 10:33:41 +0700

    Fix logic handlePaddleCollision

--- src/main/java/com/nhom12/arkanoid/logic/CollisionManager.java
+++ src/main/java/com/nhom12/arkanoid/logic/CollisionManager.java
@@ -5,18 +5,54 @@ import com.nhom12.arkanoid.utils.Constants;
 
 
 public class CollisionManager {
-    // Kiểm tra bóng va chạm với paddle;
-    public boolean checkBallHitsPaddle(Ball ball, Paddle paddle) {
-        double ballBottom = ball.getY() + ball.getRadius();
-        double ballLeft = ball.getX() - ball.getRadius();
-        double ballRight = ball.getX() + ball.getRadius();
 
-        if ((ballBottom >= paddle.getY()) && (ball.getY() <= paddle.getY() + 5)) {
-            if (ballLeft >= paddle.getX() && ballRight <= paddle.getY()) {
-                return true;
-            }
+    // Handle bóng va chạm với paddle;
+    public void handlePaddleCollision(Ball ball, Paddle paddle) {
+
+        //Bóng
+        double ballX = ball.getX();
+        double ballY = ball.getY();
+        double ballRadius = ball.getRadius();
+
+        //Thanh đỡ
+        double paddleX = paddle.getX();
+        double paddleY = paddle.getY();
+        double paddleWidth = paddle.getWidth();
+        double paddleHeight = paddle.getHeight();
+
+        //Tìm điểm trên thanh paddle mà nó gần với tâm nhất
+        double closestX;
+        if (ballX < paddleX) {
+            // Nếu tâm bóng ở bên trái của thanh đỡ thì điểm gần nhất là cạnh trái
+            closestX = paddleX;
+        } else if (ballX > paddleX + paddleWidth) {
+            // Nếu tâm bóng ở bên phải của thanh đỡ thì điểm gần nhất là cạnh phải
+            closestX = paddleX + paddleWidth;
+        } else {
+            // Nếu tâm bóng nằm giữa cạnh trái và phải thì điểm gần nhất có cùng tọa độ X với bóng
+            closestX = ballX;
         }
-        return false;
+
+        //Tương tự với trục Y
+        double closestY;
+        if (ballY < paddleY) {
+            closestY = paddleY;
+        } else if (ballY > paddleY + paddleHeight) {
+            closestY = paddleY + paddleHeight;
+        } else {
+            closestY = ballY;
+        }
+
+        double distanceX = ballX - closestX;
+        double distanceY = ballY - closestY;
+
+        double distance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY));
+
+        boolean isColliding = distance < ballRadius;
+
+        if (isColliding) {
+            ball.reverseY();
+        }
     }
 
     // kiểm tra xem ball có chạm brick
