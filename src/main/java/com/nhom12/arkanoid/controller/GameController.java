92434e5006e70653b99dd3717b9105203822db3b
hungdo2006 <24020149@vnu.edu.vn>
2025-10-17 15:49:59 +0700

    Tạo vòng lặp game và khởi tạo các biến cần thiết

--- src/main/java/com/nhom12/arkanoid/controller/GameController.java
+++ src/main/java/com/nhom12/arkanoid/controller/GameController.java
@@ -1,4 +1,51 @@
 package com.nhom12.arkanoid.controller;
 
+import com.nhom12.arkanoid.logic.GameEngine;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
 public class GameController {
+
+    @FXML
+    private Canvas gameCanvas;
+    @FXML
+    private Text scoreText;
+    @FXML
+    private Text livesText;
+    @FXML
+    private Text messageText;
+
+    private GraphicsContext gc;
+    private GameEngine gameEngine;
+    private AnimationTimer gameLoop;
+
+    private boolean isLeftKeyPressed = false;
+    private boolean isRightKeyPressed = false;
+
+    @FXML
+    public void initialize() {
+        gc = gameCanvas.getGraphicsContext2D();
+        gameEngine = new GameEngine();
+
+        // Tạo vòng lặp game
+        gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                //Update vị trí paddle
+                updatePaddlePosition();
+                //Cập nhật logic game
+                gameEngine.update();
+                //Vẽ ra màn hình
+                render();
+            }
+        };
+
+        gameLoop.start();
+    }
 }
