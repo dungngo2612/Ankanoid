b3a0ec092f3914a66eec0047b9f0f5dce0eaafe5
hungdo2006 <24020149@vnu.edu.vn>
2025-10-17 09:32:29 +0700

    Fix logic handleWallCollision

--- src/main/java/com/nhom12/arkanoid/logic/CollisionManager.java
+++ src/main/java/com/nhom12/arkanoid/logic/CollisionManager.java
@@ -1,18 +1,18 @@
 package com.nhom12.arkanoid.logic;
 
 import com.nhom12.arkanoid.model.*;
-
+import com.nhom12.arkanoid.utils.Constants;
 
 
 public class CollisionManager {
     // Kiểm tra bóng va chạm với paddle;
-    public  boolean checkBallHitsPaddle(Ball ball, Paddle paddle) {
+    public boolean checkBallHitsPaddle(Ball ball, Paddle paddle) {
         double ballBottom = ball.getY() + ball.getRadius();
         double ballLeft = ball.getX() - ball.getRadius();
         double ballRight = ball.getX() + ball.getRadius();
 
-        if ( (ballBottom >= paddle.getY()) && (ball.getY() <= paddle.getY() + 5) ) {
-            if(ballLeft >= paddle.getX() && ballRight <= paddle.getY()) {
+        if ((ballBottom >= paddle.getY()) && (ball.getY() <= paddle.getY() + 5)) {
+            if (ballLeft >= paddle.getX() && ballRight <= paddle.getY()) {
                 return true;
             }
         }
@@ -20,14 +20,14 @@ public class CollisionManager {
     }
 
     // kiểm tra xem ball có chạm brick
-    public boolean checkBallHitsBrick(Ball ball, Brick brick){
+    public boolean checkBallHitsBrick(Ball ball, Brick brick) {
 
         double ballBottom = ball.getY() + ball.getRadius();
         double ballLeft = ball.getX() - ball.getRadius();
         double ballRight = ball.getX() + ball.getRadius();
 
-        if ( (ballBottom >= brick.getY()) && (ball.getY() <= brick.getY()) ) {
-            if(ballLeft >= brick.getX() && ballRight <= brick.getY()){
+        if ((ballBottom >= brick.getY()) && (ball.getY() <= brick.getY())) {
+            if (ballLeft >= brick.getX() && ballRight <= brick.getY()) {
                 return true;
             }
         }
@@ -35,15 +35,13 @@ public class CollisionManager {
     }
 
     // Nếu ball chạm tường trái, phải hay là bên trên sẽ bị bật lại
-    public void handleWallCollision(Ball ball, double screenWidth, double screenHeight) {
-        double ballLeft = ball.getX() - ball.getRadius();
-        double ballRight = ball.getY() + ball.getRadius();
-
-        if(ballLeft <= 0 || ballRight >= screenWidth) {
+    public void handleWallCollision(Ball ball) {
+        // Tường trái và phải
+        if (ball.getX() - ball.getRadius() <= 0 || ball.getX() + ball.getRadius() >= Constants.SCENE_WIDTH) {
             ball.reverseX();
         }
-
-        if(ball.getY() - ball.getRadius() <= 0) {
+        // Tường trên
+        if (ball.getY() - ball.getRadius() <= 0) {
             ball.reverseY();
         }
     }
