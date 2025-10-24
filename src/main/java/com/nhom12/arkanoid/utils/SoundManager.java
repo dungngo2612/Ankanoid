69199ebe8887511cbc338c5bfcd2d405973b4f80
vinhsday <24020365@vnu.edu.vn>
2025-10-24 22:54:30 +0700

    Test sound for playing

--- src/main/java/com/nhom12/arkanoid/utils/SoundManager.java
+++ src/main/java/com/nhom12/arkanoid/utils/SoundManager.java
@@ -4,7 +4,6 @@ import javafx.scene.media.AudioClip;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
-import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
 
@@ -12,15 +11,24 @@ public class SoundManager {
     private static SoundManager instance;
 
     private MediaPlayer backgroundMusic;
+    private MediaPlayer playingMusic;
     private HashMap<String, AudioClip> effects;
 
     private SoundManager() {
         effects = new HashMap<>();
+        effects.put("hit_brick", new AudioClip(getClass().getResource("/Sound/hitbrick.mp3").toExternalForm()));
+        effects.put("button_click", new AudioClip(getClass().getResource("/Sound/buttonclick.mp3").toExternalForm()));
+        effects.put("hit_paddle", new AudioClip(getClass().getResource("/Sound/hitpaddle.mp3").toExternalForm()));
+        effects.put("ball_bounce", new AudioClip(getClass().getResource("/Sound/ballbounce.mp3").toExternalForm()));
+
         // Tải nhạc nền
         try {
             URL url = getClass().getResource("/Sound/background.m4a");
+            URL url1 = getClass().getResource("/Sound/detective.mp3");
             backgroundMusic = new MediaPlayer(new Media(url.toExternalForm()));
+            playingMusic = new MediaPlayer(new Media(url1.toExternalForm()));
             backgroundMusic.setCycleCount(MediaPlayer.INDEFINITE);
+            playingMusic.setCycleCount(MediaPlayer.INDEFINITE);
         } catch (Exception e) {
             System.err.println("Lỗi tải nhạc nền: " + e.getMessage());
             backgroundMusic = null;
@@ -49,6 +57,18 @@ public class SoundManager {
     public void stopBackgroundMusic() {
         if (backgroundMusic != null) {
             backgroundMusic.stop();
+        }
+    }
+
+    public void playPlayingMusic() {
+        if (playingMusic != null) {
+            playingMusic.play();
+        }
+    }
+
+    public void stopPlayingMusic() {
+        if (playingMusic != null) {
+            playingMusic.stop();
         }
     }
 
