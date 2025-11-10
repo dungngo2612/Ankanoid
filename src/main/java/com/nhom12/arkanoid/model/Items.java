a0dc31859c503e1d03cf3443d92c9bf0ef145c7d
hungdo2006 <24020149@vnu.edu.vn>
2025-11-10 09:18:23 +0700

    Update ảnh item phóng to, thu nhỏ paddle

--- src/main/java/com/nhom12/arkanoid/model/Items.java
+++ src/main/java/com/nhom12/arkanoid/model/Items.java
@@ -40,7 +40,7 @@ public class Items {
         ItemType[] itemType = ItemType.values();
         Random rand = new Random();
 //        this.type = itemType[rand.nextInt(itemType.length)];
-        this.type = ItemType.LASER_PADDLE;
+        this.type = ItemType.PADDLE_SHRINK;
     }
 
     public void removeItem() {
